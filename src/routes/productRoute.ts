1d54328d45f2c6c23555d91b4a54f85744a0e686
Shahil-Arjientek <shahil@arijentek.com>
2025-12-15 14:40:01 +0530

    feat: Add product controllers for CRUD operations

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/", getProducts);
-router.get("/:id", 
+router.get("/:id", getProductById)
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
 
